64077d1b88dbd08ac79398a626a7e16b5235817f
Parveen Kaur <parveendhanoa1225@gmail.com>
2024-10-31 14:28:46 +0800

    first commit

---
+++ banking_system_main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int new;
+
+int main()
+{
+    int mainmenu = 0;
+
+    while(1)
+    {
+        printf("----------------------------------------------------------\n");
+        printf("(1) Create New Bank Account\n");
+        printf("(2) Delete Bank Account\n");
+        printf("(3) Deposit\n");
+        printf("(4) Withdrawal\n");
+        printf("(5) Remitance\n");
+        printf("(6) Exit\n");
+        printf("----------------------------------------------------------\n");
+        scanf("%d", &mainmenu);
+
+        if (mainmenu == 1)
+        {
+
+        }
+    }
+    return 0;
+}
